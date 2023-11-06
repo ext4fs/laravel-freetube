4bac82e6171c450c57bb231463d9f1082d488828
ext4fs <wa6gtzjmwi8nnqur@gmail.com>
2023-11-06 18:13:40 +0300

    add jwt auth and modify auth config and user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,24 @@ class User extends Authenticatable
     public function comments() {
         return $this->hasMany(Comment::class, 'author_id', 'id');
     }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
